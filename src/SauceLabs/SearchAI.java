661239460a7e786a3da1c6b11393f6d591cf9d5a
ddnc1 <ddnc1@LAPTOP-LS9EEU1T>
2025-02-16 13:43:44 +0530

    Search AI

---
+++ src/SauceLabs/SearchAI.java
@@ -0,0 +1,35 @@
+package SauceLabs;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchAI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\driver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.navigate().to("https://www.google.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+		
+		WebElement searchTextbox=driver.findElement(By.id("APjFqb"));
+		searchTextbox.sendKeys("AI");
+		
+		WebElement searchButton=driver.findElement(By.name("btnK"));
+		searchButton.click();
+		
+//		if(driver.findElement(By.xpath("//div[text()='reCAPTCHA']")).isDisplayed())
+//		{
+			WebElement reCap=driver.findElement(By.xpath("//div[@class='recaptcha-checkbox-border']"));
+			reCap.click();
+//		}
+	}
+
+}
